9b94d4a2d0d65755228519945329d9d26ee5f45d
Johan Yindou <jyindou@gmail.com>
2024-01-31 14:49:25 +0100

    Mise en relation du controller avec la vue

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(
+        Request $request,
+    ): Response
     {
+        $user = $this->getUser();
+
+        if (!$user) {
+            // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
+            return $this->redirectToRoute('app_login');
+        }
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $user,
         ]);
     }
 }
