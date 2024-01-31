4369e59760e35d182814ce18bd3f0812187b263b
Michel Bondonga <bondongamichel@gmail.com>
2024-01-31 11:37:36 +0100

    Affichage des images dans le template home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
+use App\Repository\ImageRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(
+        ImageRepository $imageRepository
+    ): Response
     {
+
+        $images = $imageRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'images' => $images,
+
         ]);
     }
 }
