aa4eb99f7610fa103ce776109d5f010144ea8438
Johan Yindou <jyindou@gmail.com>
2024-02-01 12:55:02 +0100

    Améloration de la logique du controller + modification du formulaire en utilisant celui de symfony

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -34,28 +34,39 @@ class ProfileController extends AbstractController
     ): Response
     {
         $user = $this->getUser();
-        // Faire comme dans Picture controller
-        $userForm = $this->createForm(ProfileType::class, $user);
-        $userForm->handleRequest($request);
-        if ($userForm->isSubmitted() && $userForm->isValid()) {
-            $user = $userForm->getData();
-            $user->setUsername($userForm->get('username')->getData());
-            $user->setDescription($userForm->get('description')->getData());
-            /*
-            Si on veux modifer la photo de profil
-            $user->setProfilePicture($userForm->get('profilePicture')->getData());
-            */
-            
-            $em->persist($user);
-            $em->flush();
-            $this->addFlash('success', "Votre profil a bien été mis à jour");
-            return $this->redirectToRoute('app_profile');
-        }
 
         if (!$user) {
             // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
             return $this->redirectToRoute('app_login');
         }
+
+        $userForm = $this->createForm(ProfileType::class, $user);
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted()) {
+            if ($userForm->isValid()) {
+                $user = $userForm->getData();
+                $user->setUsername($userForm->get('username')->getData());
+                $user->setDescription($userForm->get('description')->getData());
+                /*
+            Si on veux modifer la photo de profil
+            $user->setProfilePicture($userForm->get('profilePicture')->getData());
+            */
+            // Test
+                $test = "Je suis arrivé jusqu'ici";
+                dump($test);
+
+
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('success', "Votre profil a bien été mis à jour");
+                return $this->redirectToRoute('app_profile');
+            }
+            else{
+                $this->addFlash('error', "Le formulaire n'est pas valide");
+        }
+        }
+        
+
 
         return $this->render('profile/edit-profile.html.twig', [
             'user' => $user,
