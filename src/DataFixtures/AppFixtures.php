63c7c4ac3915713ba659ab2ddbfbfa5d64d4ca3b
Alexandre <alexandre_viegas@icloud.com>
2024-02-01 11:07:54 +0100

    Mise à jour des fixtures pour qu'elles soit plus compréhensible

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,7 +19,8 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create();
 
-        // Création de plusieurs utilisateurs
+        // Users
+        $users = [];
         for ($i = 0; $i < 20; $i++) {
             $user = new User();
             $user->setUsername($faker->userName)
@@ -31,10 +32,11 @@ class AppFixtures extends Fixture
                 ->setUpdatedAt($faker->dateTimeBetween($user->getCreatedAt(), 'now'));
 
             $manager->persist($user);
-            $this->addReference('user_' . $i, $user);
+            $users[] = $user;
         }
 
-        // Création de plusieurs images
+        // Images
+        $images = [];
         for ($i = 0; $i < 30; $i++) {
             $image = new Image();
             $image->setTitle($faker->sentence)
@@ -42,69 +44,73 @@ class AppFixtures extends Fixture
                 ->setCreatedAt($faker->dateTimeBetween('-1 year', 'now'))
                 ->setUpdatedAt($faker->dateTimeBetween($image->getCreatedAt(), 'now'));
 
-            // Associez une image à un utilisateur
-            $user = $this->getReference('user_' . $faker->numberBetween(0, 9));
+            // Associate an image with a user
+            $user = $faker->randomElement($users);
             $image->setUser($user);
 
             $manager->persist($image);
-            $this->addReference('image_' . $i, $image);
+            $images[] = $image;
         }
 
-        // Création de plusieurs likes
+        // Likes
         for ($i = 0; $i < 60; $i++) {
             $like = new Like();
             $like->setCreatedAt($faker->dateTimeBetween('-1 year', 'now'));
 
-            // Associez un like à un utilisateur et une image
-            $user = $this->getReference('user_' . $faker->numberBetween(0, 9));
-            $image = $this->getReference('image_' . $faker->numberBetween(0, 9));
+            // Associate a like with a user and an image
+            $user = $faker->randomElement($users);
+            $image = $faker->randomElement($images);
             $like->setUser($user)->addImage($image);
 
             $manager->persist($like);
         }
 
-        // Création de plusieurs commentaires
+        // Comments
         for ($i = 0; $i < 60; $i++) {
             $comment = new Comment();
             $comment->setContent($faker->sentence)
                 ->setCreatedAt($faker->dateTimeBetween('-1 year', 'now'));
 
-            // Associez un commentaire à un utilisateur et une image
-            $user = $this->getReference('user_' . $faker->numberBetween(0, 9));
-            $image = $this->getReference('image_' . $faker->numberBetween(0, 9));
+            // Associate a comment with a user and an image
+            $user = $faker->randomElement($users);
+            $image = $faker->randomElement($images);
             $comment->setUser($user)->setImage($image);
 
             $manager->persist($comment);
         }
 
-        // Création de plusieurs catégories
+        // Categories
+        $categories = [];
         for ($i = 0; $i < 5; $i++) {
             $category = new Category();
             $category->setName($faker->word)
                 ->setCreatedAt($faker->dateTimeBetween('-1 year', 'now'));
 
-            // Associez une catégorie à plusieurs images
+            // Associate a category with multiple images
             for ($j = 0; $j < 1; $j++) {
-                $image = $this->getReference('image_' . $faker->numberBetween(0, 9));
+                $image = $faker->randomElement($images);
                 $category->setImage($image);
             }
 
             $manager->persist($category);
+            $categories[] = $category;
         }
 
-        // Création de plusieurs tags
+        // Tags
+        $tags = [];
         for ($i = 0; $i < 5; $i++) {
             $tag = new Tag();
             $tag->setName($faker->word)
                 ->setCreatedAt($faker->dateTimeBetween('-1 year', 'now'));
 
-            // Associez un tag à plusieurs images
+            // Associate a tag with multiple images
             for ($j = 0; $j < 3; $j++) {
-                $image = $this->getReference('image_' . $faker->numberBetween(0, 9));
+                $image = $faker->randomElement($images);
                 $tag->setImage($image);
             }
 
             $manager->persist($tag);
+            $tags[] = $tag;
         }
 
         $manager->flush();
